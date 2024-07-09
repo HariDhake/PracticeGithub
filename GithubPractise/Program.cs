d7c017af4a4e13753b261312c124927577acc0c2
HariDhake <dhakedharish6216@gmail.com>
2024-07-09 14:08:40 +0530

    Add project files.

---
+++ GithubPractise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GithubPractise;
+
+namespace GithubPractise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calci calci = new calci();
+            Console.WriteLine("ADD is -> "+calci.add(10,20));
+            Console.WriteLine("hello world");
+        }
+    }
+}
---
+++ GithubPractise/calci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubPractise
+{
+    public class calci
+    {
+        public int  add(int a,int b)
+        {
+            return a + b;
+        }
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
